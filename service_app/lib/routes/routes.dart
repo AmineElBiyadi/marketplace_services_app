5ad1ce26b2ebb1bf9e6e4bf343e69b0c41cec50a
douae-rohand <douaerohand18@gmail.com>
2026-03-10 04:42:43 +0000

    resolve merge conflict

--- service_app/lib/routes/routes.dart
+++ service_app/lib/routes/routes.dart
@@ -4,7 +4,9 @@
 //import '../screens/provider/provider_otp_screen.dart';
 //import '../screens/provider/provider_pending_screen.dart';
 //import '../screens/provider/provider_login_screen.dart';
-import '../screens/provider/provider_dashboard_screen.dart';class AppRoutes {
+import '../screens/provider/provider_dashboard_screen.dart';
+
+class AppRoutes {
   static const String home = '/home';
   static const String login = '/login';
   static const String register = '/register';
